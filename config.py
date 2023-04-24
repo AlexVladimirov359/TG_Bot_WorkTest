4a5925eeee1dd95c19ea477d6deb29b916248d89
AlexVladimirov359 <118312955+AlexVladimirov359@users.noreply.github.com>
2023-04-24 21:59:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-open_weather_token = "f8b8892630334df4ab796c1440531c2e"
-tg_bot_token = "5562781026:AAGKrmUlKo20nKecCzOIpDsUCy8nZDlDaDI"
+open_weather_token = "your_weather_token"
+tg_bot_token = "your_token"
 
 
